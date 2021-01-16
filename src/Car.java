6574d7dc38fd67304b68cb99ec7235eab19baa75
aleksandr_kulikov <alek00141@gmail.com>
2021-01-16 16:58:05 +0300

    added field color in  Car class

--- src/Car.java
+++ src/Car.java
@@ -1,5 +1,7 @@
 public class Car {
 
+    String color;
+
     @Override
     public String toString() {
         return "This is car";
--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Git");
         System.out.println("Where is my car?");
+        Car car = new Car();
     }
 }
